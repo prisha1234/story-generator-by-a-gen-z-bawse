83e0ba23ab897709543eabe44f14b34b629696a3
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-18 00:36:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+when=['A year ago','a long time ago','once upon a time', 'yesterday', '2 weeks ago', 'a month ago']
+who = ['a rabbit', 'i' 'my cat', 'the lion at the zoo','that ant in my backyard that i stepped on']
+name = ['prisha',' surbhi ','sushmita','hamsa ','abhinay ','abhijay',' dyuthi',' koko',' gigi',' aashirya',' inyan',' aaditi',' sarayu',' sresta',' adira']
+residence = ['india',' france',' US',' germany',' italy',' russia',' london ','australia',' austria',' spain',' china',' brazil',' mexico',' canada',' kenya',' denmark',' greece',' turkey',' greenland',' iceland',' switzerland']
+went = ['cinema',' school',' beach ','garden',' park',' home',' college',' slime factory',' mueseum',' library']
+happened = ['watch',' movie',' ate',' burger',' read',' a',' book',' chilled',' with',' friends',' learned',' about',' ancient',' stuff',' relaxed',' swam']
+print (random.choice(when)+','+random.choice(who)+','+random.choice(name)+','+random.choice(residence)+','+random.choice(went)+','+random.choice(happened))
